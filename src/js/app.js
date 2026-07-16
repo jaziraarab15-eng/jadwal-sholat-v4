3d1223254c8da9f5a793e873c6eaf764ca0fb368
root <root@localhost.localdomain>
2026-07-16 02:28:29 +0000

    Add prayer time calculation

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,12 @@
 import { Geolocation } from "@capacitor/geolocation";
 
+import {
+  PrayerTimes,
+  Coordinates,
+  CalculationMethod,
+  Madhab
+} from "adhan";
+
 async function mulai() {
   const kota = document.getElementById("kota");
   const nextPrayer = document.getElementById("nextPrayer");
@@ -21,8 +28,59 @@ async function mulai() {
     const lon = posisi.coords.longitude.toFixed(6);
 
     kota.textContent = `Lat: ${lat} | Lon: ${lon}`;
-    nextPrayer.textContent = "GPS Berhasil";
-    countdown.textContent = "Siap menghitung jadwal";
+
+const coordinates = new Coordinates(
+  parseFloat(lat),
+  parseFloat(lon)
+);
+
+const params = CalculationMethod.Singapore();
+params.madhab = Madhab.Shafi;
+
+const prayer = new PrayerTimes(
+  coordinates,
+  new Date(),
+  params
+);
+
+document.getElementById("fajr").textContent =
+  prayer.fajr.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+document.getElementById("sunrise").textContent =
+  prayer.sunrise.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+document.getElementById("dhuhr").textContent =
+  prayer.dhuhr.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+document.getElementById("asr").textContent =
+  prayer.asr.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+document.getElementById("maghrib").textContent =
+  prayer.maghrib.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+document.getElementById("isha").textContent =
+  prayer.isha.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+nextPrayer.textContent = "Jadwal Hari Ini";
+countdown.textContent = "Berhasil dihitung";
   } catch (err) {
     console.error(err);
     kota.textContent = "Gagal mengambil lokasi";
