56294214e309bd7902110c6e87059e379ed7886a
root <root@localhost.localdomain>
2026-07-16 09:46:38 +0000

    Tambahkan .gitignore dan hapus node_modules dari repository

--- node_modules/adhan/commitlint.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  ignores: [(message) => message.startsWith('chore(release)')],
-  rules: {
-    'footer-max-line-length': [0],
-  },
-};
