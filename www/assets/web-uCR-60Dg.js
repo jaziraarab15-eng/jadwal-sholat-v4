4da14db0ba72dfde02149d88f2b2b5ec99805d01
root <root@localhost.localdomain>
2026-07-19 06:28:18 +0000

    Poles UI home dan highlight sholat aktif

---
+++ www/assets/web-uCR-60Dg.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-CoZnhXNe.js";class o extends e{constructor(){super(),this.registerWindowListener("devicemotion","accel"),this.registerWindowListener("deviceorientation","orientation")}}export{o as MotionWeb};
